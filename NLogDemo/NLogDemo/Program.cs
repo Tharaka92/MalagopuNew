749c58eed3af7feb54bf154d86a7a8455e2a56af
Tharaka Herath <tharaka.career@gmail.com>
2017-05-08 13:19:15 +0530

    NLogDemo added.

---
+++ NLogDemo/NLogDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Out.WriteLine("Logging Demo Started.");
+                Console.Out.WriteLine("---------------------");
+                Console.Out.WriteLine("");
+
+                throw new DivideByZeroException();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.Out.WriteLine("Exception thrown and caught. Logging started");
+
+                logger.ErrorException("Exception Occured", ex);
+
+                Console.Out.WriteLine("Exception logged successfully. You can find the log file at C:/log/nLogDemo.log.");
+                Console.Out.WriteLine("");
+
+                Console.Out.WriteLine("Hit any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
